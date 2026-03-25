23fe3046f7859898d6930c6d1021c5f7e4cd8ccc
Lars Kristiansen <lars.kristiansen@unimicro.no>
2026-03-25 15:25:11 +0100

    chore(): Remove some boilerplate

---
+++ UniEventIntegration/Controllers/AltinnSubscriptionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UniEventIntegration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AltinnSubscriptionController : ControllerBase
+    {
+    }
+}
---
+++ UniEventIntegration/Controllers/WebHooksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UniEventIntegration.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WebHooksController : ControllerBase
+    {
+
+        public WebHooksController() { }
+
+        [HttpGet(Name = "Get")]
+        public string Get()
+        {
+            return "Hello from UniEventIntegration WebHooksController!";
+        }
+
+
+    }
+}
